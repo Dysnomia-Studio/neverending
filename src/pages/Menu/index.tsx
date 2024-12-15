fe5eacec1e131f38ea5e0beb761d3b3e2af892bb
Elanis <elanis@hotmail.com>
2024-12-15 21:53:14 +0100

    Add the menu note

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -19,6 +19,9 @@ export default function Menu() {
 		<div className="menu">
 			<h1><span className="clock">{clock[currClock]}</span><span className="medieval">Neve</span><span className="modern">ren</span><span className="future">ding</span><span className="clock">{clock[currClock]}</span></h1>
 
+			<p>Note: I wasn't able to finish the game on time for the gamejam, the game in its actual state isn't playable. You can take a look in "Start game" to know what was my last stable version. And take a look in the Game Design Document and TODO.md on Github to see what was left for this prototype.<br/>
+			It may be updated after HonestJam's rating period.</p>
+
 			<Link to="/game">Start game</Link>
 			<Link to="/credits">Credits</Link>
 			<a href="https://github.com/Dysnomia-Studio/neverending" target="_blank" rel="noopener noreferrer">Github</a>
